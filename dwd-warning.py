8fe997b6e11481cac92d2479bbd0f9df53f88c01
Sascha Curth <github@sascha-curth.de>
2020-06-14 21:41:23 +0100

    added getopts

--- dwd-warning.py
+++ dwd-warning.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env python3
 
 import sys
+import getopt
 import json
 import time
 import paho.mqtt.client as mqttClient
 import urllib.request, json 
 
-broker_address= "192.168.0.201"
-#broker_address= "127.0.0.1"
-port = 1883
-
 timestamp = int(time.time()) * 1000 
+broker = "127.0.0.1"
+port = 1883
+verbose = False
+url = "https://www.dwd.de/DWD/warnungen/warnapp/json/warnings.json"
+region = "112069000"
 
 def convert(jsonp):
     try:
@@ -24,21 +26,82 @@ def convert(jsonp):
     return res
 
 def on_publish(client,userdata,result):             #create function for callback
-    print("data published \n", result)
+    if (verbose is True):
+        print("data published \n", result)
     pass
 
-def main():
+def usage():
+    print("Usage: dwd_warning.py -s XXX [-v] [-b] [-p] [-u] [-r] [-h]")
+    print("-v: verbose output, default False")
+    print("-b: MQTT Broker IP, default 127.0.0.1")
+    print("-p: MQTT Broker Port, default 1883")
+    print("-r: DWD region, default 112069000 = Potsdam-Mittelmark")
+    print("-s: Victron Serial Number, mandatory, no default") 
+    print("-u: DWD warnings.json url, default https://www.dwd.de/DWD/warnungen/warnapp/json/warnings.json")
+    print("-h: shows this help")
+    pass
+
+def main(argv=None):
+    if argv is None:
+        argv = sys.argv
+    try:
+        options, prog_argv = getopt.getopt(argv[1:], "hb:p:r:s:u:v", ["help", "broker=","port=","region=","serial=","url=","verbose"])
+    except getopt.GetoptError:
+        sys.exit(1) 
+
+    for name, value in options:
+        if name == "-v":
+            global verbose
+            verbose = True
+        elif name in ("-h", "--help"):
+            usage()
+            sys.exit()
+        elif name in ("-b", "--broker"):
+            global broker
+            broker = value
+        elif name in ("-u", "--url"):
+            global url
+            url = value
+        elif name in ("-p", "--port"):
+            global port
+            port = value
+        elif name in ("-r", "--region"):
+            global region
+            region = value
+        elif name in ("-s", "--serial"):
+            global serial
+            serial = value
+            if (verbose is True):
+                print(serial)
+        else:
+            assert False, "unhandled option"
+            usage() 
+            sys.exit()
+    try:
+       serial
+       topic = 'W/' + serial + '/settings/0/Settings/CGwacs/BatteryLife/MinimumSocLimit'
+    except:
+       print("Serial Number not set, use option -s")
+       usage()
+       sys.exit()
+
+    if (verbose is True):
+        print("Using broker: ", broker, " on port: ", port)
+        print("Using DWD URL:", url, "and Region: ", region)
+        print("Topic: ", topic)
+
+
 #    with open('warnings.jsonp', encoding='utf-8') as fh:
-    with urllib.request.urlopen("https://www.dwd.de/DWD/warnungen/warnapp/json/warnings.json") as response:
+    with urllib.request.urlopen(url) as response:
         source=response.read().decode('utf-8')
         jsondata=convert(source)
         json_obj=json.loads(jsondata)
 
-    minsoc = '{"value": 30}' # Standard Minimum State of Charge - ausser Netzausfall
-    if "112069000" not in json_obj["warnings"]:
+    minsoc = '{"value": 25}' # Standard Minimum State of Charge - ausser Netzausfall
+    if region not in json_obj["warnings"]:
         print("Keine Warnung vorhanden")
     else:
-        myregion=json_obj["warnings"]["112069000"]
+        myregion=json_obj["warnings"][region]
 
         if myregion[0]["level"] in [3, 4, 5]:
             print("Alert Level ",myregion[0]["level"])
@@ -46,23 +109,24 @@ def main():
             print("End => " , myregion[0]["end"])
         
             alert_end_plus_buffer=myregion[0]["end"] + 2 * 60 * 60 * 1000
+            alert_start_minus_buffer=myregion[0]["start"] - 2 * 60 * 60 * 1000
 
-            if alert_end_plus_buffer > timestamp:
+            if alert_end_plus_buffer > timestamp or alert_start_minus_buffer > timestamp:
                 print ("Setting SOC to 50%")
                 minsoc = '{"value": 50}'
 
     pubclient = mqttClient.Client("writer")
     pubclient.on_publish = on_publish                          #assign function to callback
-    pubclient.connect(broker_address, port, 60)
+    pubclient.connect(broker, port, 60)
 
-    print("setting minsoc to ",minsoc)
-    ret = pubclient.publish('W/<placeholder>/settings/0/Settings/CGwacs/BatteryLife/MinimumSocLimit',minsoc)
+    if (verbose is True):
+        print("setting minsoc to ",minsoc)
+
+    ret = pubclient.publish(topic,minsoc)
     pubclient.disconnect()
 
-    print(ret)
+    if (verbose is True):
+        print(ret)
 
-#    print("alertbuff", alert_end_plus_buffer)
-#    print(json_obj["warnings"]["112069000"])
-    
 if __name__ == "__main__":
     main()
